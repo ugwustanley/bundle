d943638c6a5c5f8a09bd46c621f901fb0186cdf9
ugwustanley <ugwuchiagoziestanley@gmail.com>
2021-12-12 05:02:14 +0100

    added preloader to login and signup pages

--- pages/login.js
+++ pages/login.js
@@ -1,60 +1,73 @@
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import AuthImage from "../images/auth.svg";
 import Logo2 from "../images/logo-2.svg";
 import Google from "../images/icons/Google Icon.svg";
+import Preloader from "../components/preloader";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Login() {
+  const [showPreloader, setShowPreloader] = useState(true);
+
+  setTimeout(() => {
+    setShowPreloader(false);
+  }, 5000);
   return (
-    <div className="auth">
-      <div className="auth__image-container">
-        <h3>Sign In</h3>
-        <Link href="/">
-          <div className="auth__image">
-            <Image src={AuthImage} alt="auth" width={400} height={400} />
-          </div>
-        </Link>
-      </div>
-
-      <div className="auth__details">
-        <div className="auth__image">
-          <Image src={Logo2} alt="bundle logo" width={60} height={60} />
-        </div>
-        <h3>Welcome back!</h3>
-        <p>It’s good to have you back!</p>
-
-        <form className="auth__form">
-          <input
-            type="email"
-            placeholder="Email Address"
-            name="email"
-            className="auth__input"
-          />
-          <input
-            type="password"
-            placeholder="Enter Password"
-            className="auth__input"
-          />
-          <button className="auth__button auth__button--login">Sign In</button>
-        </form>
-
-        <div className="auth__line"></div>
-        <h5 className="auth__or">or</h5>
-
-        <button className="auth__button auth__button--google">
-          <div className="google__icon">
-            <Image src={Google} alt="google icon" width={25} height={25} />{" "}
-          </div>
-          <p>Sign in with Google</p>
-        </button>
-
-        <h6>
-          Don’t have an account?{" "}
-          <Link href="signup">
-            <b>Sign up</b>
+    <>
+      <AnimatePresence>{showPreloader ? <Preloader /> : null}</AnimatePresence>
+      <div className="auth">
+        <div className="auth__image-container">
+          <h3>Sign In</h3>
+          <Link href="/">
+            <div className="auth__image">
+              <Image src={AuthImage} alt="auth" width={400} height={400} />
+            </div>
           </Link>
-        </h6>
+        </div>
+
+        <div className="auth__details">
+          <div className="auth__image">
+            <Image src={Logo2} alt="bundle logo" width={60} height={60} />
+          </div>
+          <h3>Welcome back!</h3>
+          <p>It’s good to have you back!</p>
+
+          <form className="auth__form">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              className="auth__input"
+            />
+            <input
+              type="password"
+              placeholder="Enter Password"
+              className="auth__input"
+            />
+            <button className="auth__button auth__button--login">
+              Sign In
+            </button>
+          </form>
+
+          <div className="auth__line"></div>
+          <h5 className="auth__or">or</h5>
+
+          <button className="auth__button auth__button--google">
+            <div className="google__icon">
+              <Image src={Google} alt="google icon" width={25} height={25} />{" "}
+            </div>
+            <p>Sign in with Google</p>
+          </button>
+
+          <h6>
+            Don’t have an account?{" "}
+            <Link href="signup">
+              <b>Sign up</b>
+            </Link>
+          </h6>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- pages/signup.js
+++ pages/signup.js
@@ -1,71 +1,84 @@
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import AuthImage from "../images/auth.svg";
 import Logo2 from "../images/logo-2.svg";
 import Google from "../images/icons/Google Icon.svg";
+import Preloader from "../components/preloader";
+import { AnimatePresence } from "framer-motion";
 
-export default function Login() {
+export default function Signup() {
+  const [showPreloader, setShowPreloader] = useState(true);
+
+  setTimeout(() => {
+    setShowPreloader(false);
+  }, 5000);
   return (
-    <div className="auth">
-      <div className="auth__image-container">
-        <h3>Sign Up</h3>
-        <div className="auth__image">
-          <Image src={AuthImage} alt="auth" width={400} height={400} />
+    <>
+      <AnimatePresence>{showPreloader ? <Preloader /> : null}</AnimatePresence>
+      <div className="auth">
+        <div className="auth__image-container">
+          <h3>Sign Up</h3>
+          <div className="auth__image">
+            <Image src={AuthImage} alt="auth" width={400} height={400} />
+          </div>
+        </div>
+
+        <div className="auth__details">
+          <Link href="/">
+            <div className="auth__image">
+              <Image src={Logo2} alt="bundle logo" width={60} height={60} />
+            </div>
+          </Link>
+          <h3>Welcome to Bundle!</h3>
+          <p>We are very excited you are joining us!</p>
+
+          <form className="auth__form">
+            <input
+              type="text"
+              placeholder="Full Name"
+              name="name"
+              className="auth__input"
+            />
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              className="auth__input"
+            />
+            <input
+              type="password"
+              placeholder="Enter Password"
+              className="auth__input"
+            />
+            <input
+              type="password"
+              placeholder="Verify Password"
+              className="auth__input"
+            />
+            <button className="auth__button auth__button--login">
+              Sign Up
+            </button>
+          </form>
+
+          <div className="auth__line"></div>
+          <h5 className="auth__or">or</h5>
+
+          <button className="auth__button auth__button--google">
+            <div className="google__icon">
+              <Image src={Google} alt="google icon" width={25} height={25} />{" "}
+            </div>
+            <p>Sign up with Google</p>
+          </button>
+
+          <h6>
+            Already have an account?{" "}
+            <Link href="login">
+              <b>Sign in</b>
+            </Link>
+          </h6>
         </div>
       </div>
-
-      <div className="auth__details">
-        <Link href="/">
-          <div className="auth__image">
-            <Image src={Logo2} alt="bundle logo" width={60} height={60} />
-          </div>
-        </Link>
-        <h3>Welcome to Bundle!</h3>
-        <p>We are very excited you are joining us!</p>
-
-        <form className="auth__form">
-          <input
-            type="text"
-            placeholder="Full Name"
-            name="name"
-            className="auth__input"
-          />
-          <input
-            type="email"
-            placeholder="Email Address"
-            name="email"
-            className="auth__input"
-          />
-          <input
-            type="password"
-            placeholder="Enter Password"
-            className="auth__input"
-          />
-          <input
-            type="password"
-            placeholder="Verify Password"
-            className="auth__input"
-          />
-          <button className="auth__button auth__button--login">Sign Up</button>
-        </form>
-
-        <div className="auth__line"></div>
-        <h5 className="auth__or">or</h5>
-
-        <button className="auth__button auth__button--google">
-          <div className="google__icon">
-            <Image src={Google} alt="google icon" width={25} height={25} />{" "}
-          </div>
-          <p>Sign up with Google</p>
-        </button>
-
-        <h6>
-          Already have an account?{" "}
-          <Link href="login">
-            <b>Sign in</b>
-          </Link>
-        </h6>
-      </div>
-    </div>
+    </>
   );
 }
